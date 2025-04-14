99c00966bcf172b9d071907837bcbd24854765b7
fehervaryy <martindmhe@gmail.com>
2025-04-14 18:30:55 -0400

    fix package imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
+"use client";
 
-export default function Page() {
+import { Accordion, AccordionCard } from '@jutro/components';
+
+function Page() {
   return (
-    <div>
+    <div className="w-screen h-screen">
+      <p>Testing package</p>
+      <Accordion>
+        <AccordionCard title="test 1">
+          <p>test 1</p>
+        </AccordionCard>
+        <AccordionCard title="test 2">
+          <p>test 2</p>
+        </AccordionCard>
+        <AccordionCard title="test 3">
+          <p>test 3</p>
+        </AccordionCard>
+      </Accordion>
     </div>
-  );
+  )
 }
+
+export default Page;
