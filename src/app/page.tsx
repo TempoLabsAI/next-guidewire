b2dd802dad76d77a4466ae0cff2fae32e9ae8056
fehervaryy <martindmhe@gmail.com>
2025-04-16 10:51:36 -0400

    clean page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,8 @@
 "use client";
 
-import { Accordion, AccordionCard } from '@jutro/components';
-
 function Page() {
   return (
     <div className="w-screen h-screen">
-      <p>Testing package</p>
-      <Accordion>
-        <AccordionCard title="test 1">
-          <p>test 1</p>
-        </AccordionCard>
-        <AccordionCard title="test 2">
-          <p>test 2</p>
-        </AccordionCard>
-        <AccordionCard title="test 3">
-          <p>test 3</p>
-        </AccordionCard>
-      </Accordion>
     </div>
   )
 }
