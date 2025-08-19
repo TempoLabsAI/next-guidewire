43e715e35384ba4eb5321124693b339325ced1c7
Pritish J <jadhav.pritish@gmail.com>
2025-08-19 13:18:16 -0400

    Remove support for Tempo Dev tools for Guidewire

--- next.config.js
+++ next.config.js
@@ -2,22 +2,12 @@
 
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com'],
+      domains: ["images.unsplash.com"],
     },
     sassOptions: {
-        includePaths: ['./node_modules'],
+      includePaths: ["./node_modules"],
     },
-};
-
-if (process.env.NEXT_PUBLIC_TEMPO) {
-    nextConfig["experimental"] = {
-        // NextJS 13.4.8 up to 14.1.3:
-        // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-        // NextJS 14.1.3 to 14.2.11:
-        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
-
-        // NextJS 15+ (Not yet supported, coming soon)
-    }
-}
-
-module.exports = nextConfig;
+  };
+  
+  module.exports = nextConfig;
+  
